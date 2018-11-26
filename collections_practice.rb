31f68c9f4f9ae52b6be84178828dfa7362ee6006
Andrew Johnson <ajohnson.uva@gmail.com>
2018-11-26 15:54:46 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -19,3 +19,11 @@ def swap_elements(array)
   array[2] = two
   return array
 end
+
+def swap_elements_from_to(array, index, destination_index)
+  x = array[index]
+  y = array[destination_index]
+  array[index] = y
+  array[destination_index] = x
+  return array
+end
