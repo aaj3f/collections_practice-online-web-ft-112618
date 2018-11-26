804aa6f06352ca7f4967385c282fdacad510d6f6
Andrew Johnson <ajohnson.uva@gmail.com>
2018-11-26 15:42:40 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,15 +5,7 @@ def sort_array_asc(array)
 end
 
 def sort_array_desc(array)
-  def <=>(obj, other_obj)
-    if obj < other_obj
-      -1
-    elsif obj > other_obj
-      1
-    else
-      0
-    end
-  array.sort
+  array.sort.reverse
 end
 
 def sort_array_char_count
